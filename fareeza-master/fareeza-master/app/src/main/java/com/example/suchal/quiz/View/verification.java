e72fa6e12de8b2d931352fe1ee9dd7452b473630
RanaHashim <RanaHashim@users.noreply.github.com>
2018-08-31 18:53:27 +0500

    Add files via upload

---
+++ fareeza-master/fareeza-master/app/src/main/java/com/example/suchal/quiz/View/verification.java
@@ -0,0 +1,15 @@
+package com.example.suchal.quiz.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.suchal.quiz.R;
+
+public class verification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
